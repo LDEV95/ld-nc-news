67d0d0101182621b272110fea69079ba1f43e3e2
LDEV95 <lawrencefanutzadavis@gmail.com>
2023-10-23 15:07:59 +0100

    committing new file structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h1>NC NEWS</h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/articles">Articles</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/topics">Topics</Link>
+          </li>
+          <li>
+            {" "}
+            <Link to="/users">Users</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
